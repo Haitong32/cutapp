d01eceda4f4fcca7ab05e1f916704b1b249a39e0
Haitong32 <zhenh522@newschool.edu>
2017-05-09 16:50:47 -0400

    final

---
+++ script-hairstylelist.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $('body').css('display', 'none');
+  $('body').fadeIn(1000);
+  $("#sidebar").hide();
+  $("#star3").hide();
+  $("#star1").on('click',function(){
+    $('#star1').toggle();
+    $('#star3').toggle();
+  });
+  $("#star3").on('click',function(){
+    $('#star1').toggle();
+    $('#star3').toggle();
+  });
+  $("#oval").click(function(){
+         $("#sidebar").slideToggle()
+           return false;
+       });
+       $("#me").click(function(){
+              $("#sidebar").slideToggle()
+                return false;
+            });
+   $("#logo2").click(function(){
+              $("#sidebar").slideToggle()
+                return false;
+            });
+
+  });
+;
